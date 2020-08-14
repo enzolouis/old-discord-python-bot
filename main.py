19e01be9a4e5ea5e7a8a8f46f9f084d0dcf36f7b
EmberOn <ember.contactme@gmail.com>
2020-08-14 06:42:33 +0200

    Bug repet main.py...

--- main.py
+++ main.py
@@ -60,52 +60,6 @@ async def prefix(ctx, prefix=None):
     cursor.execute("UPDATE zel_prefix SET prefix = ? WHERE prefix_guild_id = ?", (prefix, guild_id))
   con.commit()
 
-""" GET PREFIX """
-default_prefix = ['/']
-
-async def cmd_prefix(bot, message):
-    if message.guild:
-      # sur un serveur, préfix custom
-      con = sqlite3.connect("data.db")
-      cursor = con.cursor()
-      cursor.execute("SELECT prefix FROM zel_prefix WHERE prefix_guild_id = ?", (message.guild.id, ))
-      prefix = cursor.fetchone() # ("!", )
-
-      return default_prefix if prefix is None else prefix[0]
-
-    else:
-      # en MP, préfix par défaut !
-      return default_prefix
-
-# Initialize bot
-bot = commands.Bot(command_prefix=cmd_prefix)
-bot.remove_command("help")
-
-""" PREFIX COMMAND """
-@bot.command()
-@commands.guild_only()
-async def prefix(ctx, prefix=None):
-  if prefix is None:
-      return await ctx.send(embed=discord.Embed(title=f"Prefix on this server is {bot.command_prefix(bot, ctx.message)}"))
-  
-  # first argument in case if a user type /prefix "lol d do ifnjef", prefix will be lol
-  prefix = prefix.split()[0]
-  
-  if len(prefix) > 5:
-          return await ctx.send(f"Prefix `{prefix}` too big (5 char maximum)")
-
-  con = sqlite3.connect("data.db")
-  cursor = con.cursor()
-  
-  guild_id = ctx.guild.id
-  cursor.execute("SELECT prefix FROM zel_prefix WHERE prefix_guild_id = ?", (guild_id, ))
-  result = cursor.fetchone()
-  
-  if result is None:
-    cursor.execute("INSERT INTO zel_prefix VALUES(?, ?)", (guild_id, prefix))
-  else:
-    cursor.execute("UPDATE zel_prefix SET prefix = ? WHERE prefix_guild_id = ?", (prefix, guild_id))
-  con.commit()
 
 EMBER_ID = 418154142175854613
 ZED_ID = 327074335238127616
